9703748d90c12a317408e554b765e6810c2773a7
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-11-09 20:19:20 -0300

    Colocando em prática 1.1 finalizado

---
+++ ColocandoEmPratica.js
@@ -0,0 +1,31 @@
+// DESAFIO: UTILIZAR OS OPERADORES ARITMÉTICOS VISTO EM AUTLA PARA RESOLVER AS TAREFAS:
+
+// 1)
+var soma
+soma = 4 + 5
+console.log("Soma: ", soma)
+
+// 2)
+var subtracao
+subtracao = -5 - 3
+console.log("Subtração: ", subtracao)
+
+// 3)
+var divisao
+divisao = 80 / 3
+console.log ("Divisão: ", divisao)
+var restoInteiro
+restoInteiro = 80 % 3
+console.log ("Resto Inteiro: ", restoInteiro)
+
+// 4)
+var num1 = 4
+num1++
+soma = 4 + 5 + num1
+console.log("Soma com incremento na variável: ", soma)
+
+// 5)
+var num2 = 4
+num2--
+subtracao = -5 - 3 - num2
+console.log("Subtração com decremento na varável: ", subtracao)
